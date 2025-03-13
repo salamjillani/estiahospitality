84d36cdd25e73be88b3cd4e779e1cfa251e140c5
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2025-03-13 05:09:51 +0500

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,30 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
-      '/socket.io': {
-        target: 'ws://localhost:5000', 
+      "/socket.io": {
+        target: "ws://localhost:5000",
         ws: true,
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/socket.io/, '')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/socket.io/, ""),
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
   },
 });
